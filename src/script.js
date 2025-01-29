bdfd51958b8fbec18700c1616bfa1354cd7b6274
Oskar Hosken <oskarhosken@gmail.com>
2025-01-30 10:09:19 +1100

    Letters are placed in cubes when pressed.

--- src/script.js
+++ src/script.js
@@ -6,9 +6,13 @@
 const gameState = {
     word: 'oskar',
     guesses: [],
-    maxGuesses: 5,
+    currentGuess: "",
+    maxGuesses: 6,
     gameOver: false
 }
+
+// The state we are replacing
+let currentState = gameState
 
 // Setting up the grid
 document.addEventListener('DOMContentLoaded', () => {
@@ -24,20 +28,64 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Creates an array of cubes
     const createGrid = (rows, cols) => {
-        return Array.from({ length: rows * cols }, createCube);
+        return Array.from({ length: rows * cols }, createCube)
     }
 
     // Adds all cubes to our grid
     const cubes = createGrid(6, 5)
-    cubes.forEach(cube => wordGrid.appendChild(cube))
+    cubes.forEach((cube, index) => {
+        // Adding an id to each cube so we can track each one
+        //? Not sure if this is legal for functional
+        cube.id = `cube-${index}`
+        wordGrid.appendChild(cube)
+    })
 })
 
+// Function to add a letter to the current cube
+const addLetter = (state, letter) => {
+    // Current row is representative by the number of guesses user has made
+    const currRow = state.guesses.length
+    // And current column is representative by the current guess
+    const currCol = state.currentGuess.length
+    // This is the cube we need to place the letter in
+    const cubeId = currRow + currCol
+    const cube = document.getElementById(`cube-${cubeId}`)
+    // Updating HTML.
+    //! Check later for functional
+    cube.innerHTML = letter.toUpperCase()
+
+    return {
+        ...state,
+        // Adding the letter to the end of the word
+        currentGuess: state.currentGuess + letter
+    }
+}
+
+// Function to update the state and check the guesses.
+const checkGuess = (state, guess) => {
+    //! TODO
+}
+
+// Removing last character from current guess
+const decreaseGuess = (state) => {
+    return {
+        ...state,
+        currentGuess: state.currentGuess.slice(0, -1)
+    }
+}
+
 // Testing out key events
-document.addEventListener('keypress', (event) => {
+document.addEventListener('keydown', (event) => {
     const keyName = event.key
     const keyCode = keyName.toUpperCase().charCodeAt(0)
-    // Ensuring a valid key is pressed.
-    if (keyCode >= 65 && keyCode <= 90) {
-        console.log(keyName)
+    // Checking if delete key was pressed
+    if ((keyName === "Backspace" || keyName === "Delete")) {
+        // Updating current game state
+        currentState = decreaseGuess(currentState)
+    // Ensuring a valid alphabetic key was pressed
+    } else if (keyCode >= 65 && keyCode <= 90) {
+        console.log(keyCode)
+        // Need to place the letters in their div elements
+        currentState = addLetter(currentState, keyName)
     }
 })
